dcb9b4b1910b9549dc2866e4a0d644ed716a2634
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-30 18:08:30 +0100

    Migrate build to shared convention plugins setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
-plugins {
-    val nmcpVersion = "1.2.0"
-    val mavenPluginDevVersion = "1.0.3"
+plugins { id("org.gradlex.maven-plugin-development") version "1.0.3" }
 
-    id("com.gradleup.nmcp") version nmcpVersion
-    id("com.gradleup.nmcp.aggregation") version nmcpVersion
-    id("org.gradlex.maven-plugin-development") version mavenPluginDevVersion
-    id("maven-publish")
-    id("signing")
-    id("checkstyle")
-}
-
-group = "org.gradlex"
 version = "1.2"
 
 val mvnVersion = "3.9.11"
@@ -21,8 +10,17 @@ dependencies {
     compileOnly("org.apache.maven:maven-core:$mvnVersion")
     compileOnly("org.apache.maven:maven-plugin-api:$mvnVersion")
     compileOnly("org.apache.maven.plugin-tools:maven-plugin-annotations:3.15.2")
+}
 
-    nmcpAggregation(project(path))
+configurations.api {
+    withDependencies { clear() } // remove gradleApi() dependency
+}
+
+tasks.javadoc {
+    options {
+        this as StandardJavadocDocletOptions
+        tags("goal:a:Goal:", "requiresProject:a:Requires Project:", "threadSafe:a:Thread Safe:")
+    }
 }
 
 mavenPlugin {
@@ -31,88 +29,9 @@ mavenPlugin {
     helpMojoPackage = "org.gradlex.maven.gmm"
 }
 
-java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
-    withSourcesJar()
-    withJavadocJar()
-}
-
-tasks.compileJava {
-    options.release = 8
-    options.compilerArgs.add("-Werror")
-}
-
-tasks.javadoc {
-    options {
-        this as StandardJavadocDocletOptions
-        addStringOption("Xdoclint:all,-missing", "-Xwerror")
-        tags(
-            "goal:a:Goal:",
-            "requiresProject:a:Requires Project:",
-            "threadSafe:a:Thread Safe:"
-        )
+publishingConventions {
+    mavenCentral {
+        displayName = mavenPlugin.name
+        description = mavenPlugin.description
     }
-}
-
-@Suppress("UnstableApiUsage")
-testing.suites.named<JvmTestSuite>("test") {
-    useJUnitJupiter()
-    dependencies {
-        implementation(gradleTestKit())
-        implementation("org.assertj:assertj-core:3.27.6")
-    }
-}
-
-publishing {
-    publications.register<MavenPublication>("mavenPlugin") {
-        from(components["java"])
-        pom {
-            name = mavenPlugin.name
-            description = mavenPlugin.description
-            url = "https://github.com/gradlex-org/gradle-module-metadata-maven-plugin"
-            licenses {
-                license {
-                    name = "Apache License, Version 2.0"
-                    url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                }
-            }
-            scm {
-                connection = "scm:git:git://github.com/gradlex-org/gradle-module-metadata-maven-plugin.git"
-                developerConnection = "scm:git:git://github.com/gradlex-org/gradle-module-metadata-maven-plugin.git"
-                url = "https://github.com/gradlex-org/gradle-module-metadata-maven-plugin"
-            }
-            developers {
-                developer {
-                    name = "Jendrik Johannes"
-                    email = "jendrik@gradlex.org"
-                }
-            }
-        }
-    }
-}
-
-signing {
-    if (providers.gradleProperty("sign").getOrElse("false").toBoolean()) {
-        useInMemoryPgpKeys(
-            providers.environmentVariable("SIGNING_KEY").getOrNull(),
-            providers.environmentVariable("SIGNING_PASSPHRASE").getOrNull()
-        )
-        sign(publishing.publications["mavenPlugin"])
-    }
-}
-
-nmcpAggregation {
-    centralPortal {
-        username = providers.environmentVariable("MAVEN_CENTRAL_USERNAME")
-        password = providers.environmentVariable("MAVEN_CENTRAL_PASSWORD")
-        publishingType = "AUTOMATIC" // "USER_MANAGED"
-    }
-}
-
-checkstyle {
-    configDirectory = layout.projectDirectory.dir("gradle/checkstyle")
-}
-
-tasks.checkstyleMain {
-    exclude("**/HelpMojo.java")
 }
