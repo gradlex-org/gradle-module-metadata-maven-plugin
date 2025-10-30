21fd98c7cc537af8c394f145381643850665a4b6
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-30 18:09:27 +0100

    Format code

--- src/main/java/org/gradlex/maven/gmm/GradleModuleMetadataMojo.java
+++ src/main/java/org/gradlex/maven/gmm/GradleModuleMetadataMojo.java
@@ -1,21 +1,12 @@
-/*
- * Copyright the GradleX team.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
 package org.gradlex.maven.gmm;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import javax.inject.Inject;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -24,13 +15,6 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
-
-import javax.inject.Inject;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
 
 /**
  * Goal that generates Gradle Module Metadata.
@@ -99,7 +83,8 @@ public class GradleModuleMetadataMojo extends AbstractMojo {
         try {
             String pomContent = new String(Files.readAllBytes(pomFile.toPath()));
             if (!pomContent.contains(marker)) {
-                String newContent = pomContent.replaceFirst("</modelVersion>", "</modelVersion> <!-- " + marker + " -->");
+                String newContent =
+                        pomContent.replaceFirst("</modelVersion>", "</modelVersion> <!-- " + marker + " -->");
                 Files.write(pomFile.toPath(), newContent.getBytes());
             }
         } catch (IOException e) {
